2d66db07c78977a22eac3762ccc089703c4ecd1e
Syed Arsalan Khan <khansyed231am@gmail.com>
2023-03-05 17:57:22 +0500

    Securing my connection

--- Social Media Backend/app.js
+++ Social Media Backend/app.js
@@ -2,17 +2,14 @@ import express from "express";
 import mongoose from "mongoose";
 import router from "./routes/user-routes";
 import blogRouter from "./routes/blog-routes";
+import MyEnv from "dotenv";
 const app = express();
-
+MyEnv.config();
 app.use(express.json());
 app.use("/api/user", router);
 app.use("/api/blog", blogRouter);
 mongoose
-  .connect(
-    "mongodb+srv://arsalan:gHpIuUVn2Z3DTQYI@blogpost.j5nmy3g.mongodb.net/BlogPost?retryWrites=true&w=majority"
-  )
+  .connect(process.env.CONNECTION_URI)
   .then(() => app.listen(5000))
   .then(() => console.log("Connected to DB and listening to 5000"))
   .catch((err) => console.log(err));
-
-//gHpIuUVn2Z3DTQYI
